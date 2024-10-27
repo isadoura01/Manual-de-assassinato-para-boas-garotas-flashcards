7299c7b53c87e8296dfdb3c0ada576dd694a733e
isadoura01 <138601552+isadoura01@users.noreply.github.com>
2024-10-27 00:02:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Manual de Assassinato Para Boas Garotas',
+    'Qual é o objetivo da personagem principal, Pip, no início do livro?',
+    'Pip quer investigar um crime antigo que ocorreu em sua cidade e que ela acredita não ter sido resolvido corretamente. Seu objetivo é provar que Sal Singh, o principal suspeito, pode ser inocente.'
+)
+
+criaCartao(
+    'Manual de Assassinato Para Boas Garotas',
+    'Que impacto a investigação tem sobre a visão de Pip em relação à sua cidade?',
+    'Conforme a investigação avança, Pip descobre segredos sombrios sobre as pessoas ao seu redor, o que faz com que ela passe a ver sua cidade e seus moradores de maneira diferente, com mais desconfiança e cautela.'
+)
+
+criaCartao(
+    'Manual de Assassinato Para Boas Garotas',
+    'Por que Pip decide investigar o caso de Andie Bell?',
+    'Pip decide investigar porque não acredita que Sal Singh seja culpado e quer entender o que realmente aconteceu, além de ajudar sua comunidade a descobrir a verdade.'
+)
+
+criaCartao(
+    'Manual de Assassinato Para Boas Garotas',
+    'Como Pip organiza suas investigações ao longo do livro?',
+    'Pip organiza suas investigações de forma muito detalhada, mantendo registros em anotações e coletando evidências para montar um quadro completo dos acontecimentos.'
+)
+
+criaCartao(
+    'Manual de Assassinato Para Boas Garotas',
+    'Qual é o papel de Ravi Singh na história?',
+    'Ravi Singh é o irmão de Sal Singh, o principal suspeito do crime. Ele ajuda Pip em sua investigação, pois também quer provar a inocência do irmão.'
+)
+
+criaCartao(
+    'Manual de Assassinato Para Boas Garotas',
+    'Quais desafios Pip enfrenta durante sua investigação?',
+    'Pip enfrenta resistência de pessoas que não querem que o caso seja reaberto, além de ameaças e pistas falsas que complicam sua busca pela verdade.'
+)
+
+criaCartao(
+    'Manual de Assassinato Para Boas Garotas',
+    'Como o caso investigado por Pip afeta sua vida pessoal?',
+    ' A investigação começa a consumir muito do tempo de Pip, afetando suas amizades, sua relação com a família e até seu desempenho escolar, além de colocá-la em situações perigosas.'
+)
